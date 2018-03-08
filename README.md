9e5cd6f738b7abc69e13ab0a2c7d878751a5dab1
shengyu <shengyu7697@gmail.com>
2018-03-08 10:15:39 +0800

    update README.md

--- README.md
+++ README.md
@@ -2,6 +2,12 @@ PyGetMyPublicIP
 ============
 This is a program that get and show your public IP addresses.  
 
+## Features
+* Support many websites:
+    + https://myip.com.tw/
+    + https://www.rus.net.tw/myip.php
+    + https://api.ipify.org
+
 ## How to Use
 
 ```
--- get-publicip.py
+++ get-publicip.py
@@ -4,25 +4,25 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-def getMyPublicIP():
+def getMyPublicIP1():
 	r = requests.get('https://myip.com.tw/')
-	c = r.content
-	soup = BeautifulSoup(c, "lxml")
-	tag = soup.find("font").getText()
+	soup = BeautifulSoup(r.content, 'lxml')
+	tag = soup.find('font').getText()
 	return tag
 
 def getMyPublicIP2():
 	r = requests.get('https://www.rus.net.tw/myip.php')
-	c = r.content
-	soup = BeautifulSoup(c, "lxml")
-	tag = soup.find_all("td", text="您 的 IP 是")[0].find_next_sibling("td").getText()
+	soup = BeautifulSoup(r.content, 'lxml')
+	tag = soup.find_all('td', text='您 的 IP 是')[0].find_next_sibling('td').getText()
 	return tag.strip()
 	#<td class="myip" align="center" width="300">您 的 IP 是</td>
-	#<td width="190">219.87.64.222    </td>
+	#<td width="190">xx.xx.xx.xx    </td>
 
 def getMyPublicIP3():
 	return requests.get('https://api.ipify.org').text
 
 if __name__ == '__main__':
-	ip = getMyPublicIP()
-	print 'My Public IP address is: %s' % ip
+	print 'My Public IP address is:'
+	print '%s (https://myip.com.tw/)' % getMyPublicIP1()
+	print '%s (https://www.rus.net.tw/myip.php)' % getMyPublicIP2()
+	print '%s (https://api.ipify.org)' % getMyPublicIP3()
